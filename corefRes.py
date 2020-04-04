c789b5319e329cb612fc66a5d7fc421f4e717f07
mihikabairathi <mihikabairathi@gmail.com>
2020-04-04 16:24:29 -0400

    Fixed multiple occurrences in sentence.

--- corefRes.py
+++ corefRes.py
@@ -3,27 +3,17 @@ import os
 import stanfordnlp
 import string
 import copy
+
+#uncomment the stuff commented out if you want to run this file on its own.
+"""
 os.environ["CORENLP_HOME"] = "./corenlp"
 from stanfordnlp.server import CoreNLPClient
 
 nlpCore = CoreNLPClient(annotators=['tokenize','coref'], 
                         endpoint='http://localhost:9001')
 nlpCore.start()
+"""
 
-text1 = "Niki Murikinati is useless. Mihika Bairathi says she knows nothing. Bob went to the market. He met Niki there."
-text2 = "Her mom's food is amazing."
-text3 = "Her name is M. Bairathi, and she is really nice."
-text4 = "Barack Hussein Obama II (born August 4, 1961) is an American politician" +\
-        " and attorney who served as the 44th president of the United States " +\
-        "from 2009 to 2017. A member of the Democratic Party, he was the first " + \
-        "African-American president of the United States. He previously served " +\
-        "as a U.S. senator from Illinois from 2005 to 2008 and an Illinois state" +\
-        " senator from 1997 to 2004. Obama was born in Honolulu, Hawaii. After "+\
-        "graduating from Columbia University in 1983, he worked as a community " +\
-        "organizer in Chicago. In 1988, he enrolled in Harvard Law School, where " +\
-        "he was the first black person to head the Harvard Law Review. After " +\
-        "graduating, he became a civil rights attorney and an academic, teaching " +\
-        "constitutional law at the University of Chicago Law School from 1992 to 2004."
 
 def collapseWhitespace(s):
     """
@@ -102,10 +92,20 @@ def getCorefs(text, docCore, allSentences):
         sNumber = cm[0].sentenceIndex
         rightWord = " ".join(allSentences[sNumber][begin:end])
         #replace in all the others
-        for m in cm[1:]:
+        for i in range (1, len(cm)):
+            m = cm[i]
             b = m.beginIndex
             e = m.endIndex
             sIndex = m.sentenceIndex
+            if e-b != 1:
+                continue
+            if cm[i-1].sentenceIndex == sIndex or (i+1 < len(cm) and cm[i+1].sentenceIndex == sIndex):
+                seen = 0
+                for w in rightWord.split(" "):
+                    if w in newSentences[sIndex][:b]:
+                        seen = 1
+                if seen == 1 or rightWord in newSentences[sIndex][:b]:
+                    continue
             newSentences[sIndex] = newSentences[sIndex][:b] + [" "]*(e-b) + newSentences[sIndex][e:]
             newSentences[sIndex][b] = rightWord
     return newSentences
@@ -120,8 +120,45 @@ def modifyText(text, docCore):
         modifiedText += " ".join(sentence) + " "
     return collapseWhitespace(modifiedText)
 
+text4 = "The Old Kingdom is the period in the third millennium (c. 2686-2181 BC) "+\
+        "also known as the 'Age of the Pyramids' or 'Age of the Pyramid Builders' " +\
+        "as it includes the great 4th Dynasty when King Sneferu perfected the art "+\
+        "of pyramid building and the pyramids of Giza were constructed under the "+\
+        "kings Khufu, Khafre, and Menkaure. Egypt attained its first continuous peak "+\
+        "of civilization – the first of three so-called 'Kingdom' periods (followed "+\
+        "by the Middle Kingdom and New Kingdom) which mark the high points of "+\
+        "civilization in the lower Nile Valley. The term itself was coined by "+\
+        "eighteenth-century historians and the distinction between the Old Kingdom "+\
+        "and the Early Dynastic Period is not one which would have been recognized "+\
+        "by Ancient Egyptians. Not only was the last king of the Early Dynastic "+\
+        "Period related to the first two kings of the Old Kingdom, but the 'capital', "+\
+        "the royal residence, remained at Ineb-Hedg, the Ancient Egyptian name for "+\
+        "Memphis. The basic justification for a separation between the two periods "+\
+        "is the revolutionary change in architecture accompanied by the effects on "+\
+        "Egyptian society and economy of large-scale building projects. The Old Kingdom "+\
+        "is most commonly regarded as the period from the Third Dynasty through to "+\
+        "the Sixth Dynasty (2686–2181 BC). The 4th-6th Dynasties of Egypt, are "+\
+        "scarce and historians regard the history of the era as literally 'written "+\
+        "in stone' and largely architectural in that it is through the monuments "+\
+        "and their inscriptions that scholars have been able to construct a history. "
+
+text2 = "Barack Hussein Obama II (born August 4, 1961) is an American politician" +\
+        " and attorney who served as the 44th president of the United States " +\
+        "from 2009 to 2017. A member of the Democratic Party, he was the first " + \
+        "African-American president of the United States. He previously served " +\
+        "as a U.S. senator from Illinois from 2005 to 2008 and an Illinois state" +\
+        " senator from 1997 to 2004. Obama was born in Honolulu, Hawaii. After "+\
+        "graduating from Columbia University in 1983, he worked as a community " +\
+        "organizer in Chicago. In 1988, he enrolled in Harvard Law School, where " +\
+        "he was the first black person to head the Harvard Law Review. After " +\
+        "graduating, he became a civil rights attorney and an academic, teaching " +\
+        "constitutional law at the University of Chicago Law School from 1992 to 2004."
+
+"""
 #replace text with the particular type
 text = text4
-docCore = nlpCore.annotate(text4)
+docCore = nlpCore.annotate(text)
 print(modifyText(text, docCore))
 nlpCore.stop()
+"""
+
