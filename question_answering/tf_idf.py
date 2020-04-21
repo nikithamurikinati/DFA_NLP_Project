ece2f2574af93d98ec01728b8662785d7b4d88ca
SunjanaK <potterfan202@gmail.com>
2020-04-21 01:13:40 -0400

    Add files via upload

--- question_answering/tf_idf.py
+++ question_answering/tf_idf.py
@@ -1,82 +1,119 @@
 import math
+import nltk
+from nltk.stem import WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
+
+print(lemmatizer.lemmatize("was"))
 #implementing tf-idf
 
-#returns vector space representation for a sentence of term frequencies 
+#returns vector space representation for a sentence of term frequencies
+# need to define global variable sentences 
 
+# nlp = spacy.load("en_core_web_sm")
 
 def calculateTermFrequencies(s):
-    s = s.split(" ")
-    d = dict()
-    for word in s:
-        if(word.lower() in d):
-            d[word.lower()] += 1
+    words = s.split(" ")
+    # print(s, words)
+    # for token in nlp(s):
+    #     words.append(token.text)
+    total = len(words)
+    d = dict() #getting the num of each word in the doc 
+    for word in words:
+        new = word.lower()
+        if(new in d):
+            d[new] += 1
         else:
-            d[word.lower()] = 1
-    vectorWords = []
+            d[new] = 1
+    print(d)
     vectorDict = dict()
-    total = 0
-    indices = set()
-    for word in s:
-        if word.lower() not in indices: #individual words
-            indices.add(word.lower())
-            vectorWords.append(word.lower())
-            total += d[word.lower()]
-    
-    for i, word in enumerate(vectorWords):
-        vectorDict[word] = d[word] / total
-        # vectorFreqs[i] = d[word] / total
+    for word in d:
+        vectorDict[word] = float(d[word]) / float(total)
 
     return vectorDict #dictionary for term freqs for a particular sentence
 
-def calculateTermFreq(sentence, t):
-    if(t.lower() in calculateTermFrequencies(sentence)):
-        return calculateTermFrequencies(sentence)[t.lower()]
+def calculateTermFreq(t, sentence):
+    word = t.lower()
+    if(word in calculateTermFrequencies(sentence)):
+        return calculateTermFrequencies(sentence)[word]
     else:
         return 0
 
 
-def calcInverseDocFrequency(sentences, t):
+def calcInverseDocFrequency(t, sentences):
+    word = t.lower()
     numDocs = 0
     for sentence in sentences:
-        if t.lower() in sentence.lower().split(" "):
+        words = calculateTermFrequencies(sentence)
+        if word in words:
             numDocs += 1
     if(numDocs > 0):
         return math.log(len(sentences) / numDocs)
     else:
         return 0
 
+
+def calcDotProduct(query, document, vocab, sentences): #an ordered list of words
+    res = 0
+    for term in vocab:
+        tf_idfQ = calculateTermFreq(term, query) * calcInverseDocFrequency(term, sentences)
+        tf_idfD = calculateTermFreq(term, document) * calcInverseDocFrequency(term, sentences)
+        res += tf_idfQ * tf_idfD
+    return res
+
+def findCosineSimilarity(query, document, vocab, sentences):
+    dotProduct = calcDotProduct(query, document, vocab, sentences)
+    #calculating ||query|| and ||document||
+    q = 0
+    d = 0
+    for term in vocab:
+        tf_idf = calculateTermFreq(term, query) * calcInverseDocFrequency(term, sentences)
+        tf_idf2 = calculateTermFreq(term, document) * calcInverseDocFrequency(term, sentences)
+        q += tf_idf ** 2
+        d += tf_idf2 ** 2
+    if(q * d == 0):
+        return 5000
+    return dotProduct / (q * d)
+
+
 def compareToOriginal(originalQ, sentences): 
     allSentences = sentences + [originalQ]
-    vectorMapper = dict() #mapping sentences to their vectors for comparison 
-    #get vocabulary 
-    vocab = set()
-    for sentence in allSentences:
-        for word in sentence.lower().split(" "):
-            vocab.add(word)
-    vocab = list(vocab) # consistently ordered set of words
-    #building vectorMapper
-    for sentence in allSentences:
-        #building the vector 
-        vector = [0 for i in range(len(vocab))]
-        vectorWords = [0 for i in range(len(vocab))]
-        ind = 0
-        for word in vocab:
-            if(word not in vectorWords):
-                vectorWords[ind] = word 
-                vector[ind] = calculateTermFreq(sentence, word) * calcInverseDocFrequency(allSentences, word)
-                ind += 1
+    #constructing vocabulary
+    vocabulary = set()
 
-        vectorMapper[sentence] = vector
+    words = originalQ.split(" ")
+    for word in words:
+        vocabulary.add(word.lower())
+    print(vocabulary)
 
-
-    origVector = vectorMapper[originalQ]
+    #comparing query with each sentence
     resultDict = dict()
-    for sentence in sentences:
-        #calculate dot product between original sentence and each of these sentences
-        res = 0
-        for i in range(len(vectorMapper[sentence])):
-            res += origVector[i] * vectorMapper[sentence][i]
-        res = res // (len(sentence.split(" ")) + len(originalQ.split(" ")))
-        resultDict[sentence] = res
+    for s in sentences:
+        resultDict[s] = findCosineSimilarity(originalQ, s, vocabulary, allSentences)
+    print(resultDict)
 
     return min(resultDict, key=resultDict.get)
+
+ 
+
+# q = "What was always a central concern for ancient Egyptians"
+# sents = ["Cheese wasn't always a central concern for the ancient Egyptians.", "Divine pardon at judgement was always a central concern for the ancient Egyptians."]
+
+# q = "Why does Sirius appear bright"
+# sents = ["The brightest star in the night sky, Sirius is recorded in the earliest astronomical records.",
+#     "Sirius appears bright because of its intrinsic luminosity and its proximity to Earth.", 
+# "Sirius (, a romanization of Greek Σείριος, Seirios, lit) is a star system and the brightest star in the Earth's night sky."]
+# print(compareToOriginal(q, sents))
+
+
+
+# tests = dict()
+# tests["What was always a central concern for ancient Egyptians"] = ["Cheese wasn't always a central concern for the ancient Egyptians.", "Divine pardon at judgement was always a central concern for the ancient Egyptians."]
+# tests["What was the first phase of the festival"] = ["The first phase of the festival was a public drama depicting the murder and dismemberment of Osiris.", "The annual festival involved the construction of Osiris Beds formed in shape of Osiris, filled with soil and sown with seed."]
+# tests["When was the government in the hands of the various nomes"] = ["The history of ancient Egypt is divided into three kingdoms and two intermediate periods.",
+# "During the intermediate periods (the periods of time between kingdoms) government control was in the hands of the various nomes (provinces within Egypt) and various foreigners.",
+# "For most parts of its long history, ancient Egypt was unified under one government."]
+
+
+
+
+# tester(tests)
